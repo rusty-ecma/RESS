01ef6aef1efb5c141945c6a4e46f18489d731a6f
Robert Masen <r@robertmasen.pizza>
2018-10-04 13:11:40 -0500

    enums w/o associated values can be copy for cheap

--- src/comments.rs
+++ src/comments.rs
@@ -12,7 +12,7 @@ pub struct Comment {
     pub kind: Kind,
     pub content: String,
 }
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Kind {
     Single,
     Multi,
