3b2ee7dfed39102bf3cbf0a2cdeacff7d5943eeb
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-22 08:33:56 +0200

    Add FAIL_COMMENTS to html comment test

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -1407,17 +1407,27 @@ mod test {
 
     #[test]
     fn tokenizer_html_comment() {
-        static COMMENTS: &[&str] = &[
+        static SUCCESS_COMMENTS: &[&str] = &[
             "<!--line feed\n",
             "<!--carriage return\r",
             "<!--crlf\r\n",
             "<!--line separator\u{2028}",
             "<!--paragraph separator\u{2029}",
         ];
-        for c in COMMENTS {
+        static FAIL_COMMENTS: &[&str] = &[
+            "<!--this will fail",
+        ];
+        for c in SUCCESS_COMMENTS {
             let mut t = Tokenizer::new(c);
             let item = t.next().unwrap();
             assert!(item.ty.is_comment());
+        }
+        for c in FAIL_COMMENTS {
+            let mut t = Tokenizer::new(c);
+            match t.next() {
+                Err(_) => continue,
+                _ => panic!("FAIL_COMMENTS should fail"),
+            };
         }
     }
 
