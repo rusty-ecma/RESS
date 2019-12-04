c3855b2ae8808f92de9eacbe8505cb3faaf91b91
Robert Masen <r.f.masen@gmail.com>
2019-12-04 11:53:33 -0600

    * clippy lints * add whitespace test to buffer

--- src/tokenizer/unicode.rs
+++ src/tokenizer/unicode.rs
@@ -58,25 +58,3 @@ pub(crate) fn is_id_continue(c: char) -> bool {
         unic_ucd_ident::is_id_continue(c)
     }
 }
-#[inline]
-pub(crate) fn is_other_whitespace(c: char) -> bool {
-    if c < '\u{2000}' {
-        false
-    } else if c >= '\u{2000}' && c <= '\u{200A}' {
-        true
-    } else if c < '\u{202F}' {
-        false
-    } else if c == '\u{202F}' {
-        true
-    } else if c < '\u{205F}' {
-        false
-    } else if c == '\u{205F}' {
-        true
-    } else if c < '\u{3000}' {
-        false
-    } else if c == '\u{3000}' {
-        true
-    } else {
-        false
-    }
-}
